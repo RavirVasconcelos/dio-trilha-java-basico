d234930a53b6589bcaff4afed7ba59879cdad9aa
Ytalo Vasconcelos <ytalovasconcelos01@gmail.com>
2024-11-08 00:26:18 -0300

    vai por favor

--- ContaBanco/src/Main.java
+++ ContaBanco/src/Main.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         ContaTerminal banco = new ContaTerminal();
 
         System.out.print("Insira o número da conta: ");
-        banco.numero = sc.nextInt();
-        sc.nextLine();
+        banco.numero = scanner.nextInt();
+        scanner.nextLine();
         System.out.print("Agência: ");
-        banco.agencia = sc.nextLine();
+        banco.agencia = scanner.nextLine();
         System.out.print("Digite seu nome: ");
-        banco.nomeCliente = sc.nextLine();
+        banco.nomeCliente = scanner.nextLine();
         System.out.print("Insira o seu saldo: ");
-        banco.saldo = sc.nextDouble();
-        sc.nextLine();
+        banco.saldo = scanner.nextDouble();
+        scanner.nextLine();
 
         System.out.printf("Olá %s , obrigado por criar uma conta em nosso banco, sua agência é %s , conta %d e seu Saldo %.2f%n", banco.nomeCliente, banco.agencia, banco.numero, banco.saldo);
 
-        sc.close();    
+        scanner.close();    
     }
 }
