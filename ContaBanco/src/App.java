fbdb7292402b008f0bd85c54aa446662d3970d64
Ytalo Vasconcelos <ytalovasconcelos01@gmail.com>
2024-11-08 00:21:43 -0300

    finalizado

--- ContaBanco/src/App.java
+++ ContaBanco/src/App.java
@@ -1,7 +1,9 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
         ContaTerminal banco = new ContaTerminal();
