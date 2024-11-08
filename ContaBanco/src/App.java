60d165ddc8b6fdedb7cf1c84166b9cf751ae186c
Ytalo Vasconcelos <ytalovasconcelos01@gmail.com>
2024-11-08 00:18:06 -0300

    iniciando o projeto

---
+++ ContaBanco/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ContaTerminal banco = new ContaTerminal();
+
+        System.out.print("Insira o número da conta: ");
+        banco.numero = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Agência: ");
+        banco.agencia = sc.nextLine();
+        System.out.print("Digite seu nome: ");
+        banco.nomeCliente = sc.nextLine();
+        System.out.print("Insira o seu saldo: ");
+        banco.saldo = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.printf("Olá %s , obrigado por criar uma conta em nosso banco, sua agência é %s , conta %d e seu Saldo %.2f%n", banco.nomeCliente, banco.agencia, banco.numero, banco.saldo);
+
+        sc.close();    
+    }
+}
---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,9 @@
+public class ContaTerminal {
+    
+    int numero = 1021;
+    String agencia;
+    String nomeCliente = "MARIO ANDRADE";
+    double saldo = 237.48;
+
+
+}
